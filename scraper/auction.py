44708f1c87501212e0ba4c0cb7f1a4a622a3b313
KJ.O <nomeansno@Live.ca>
2022-10-02 19:14:55 +0900

    several changes

--- scraper/auction.py
+++ scraper/auction.py
@@ -40,8 +40,8 @@ def build_driver():
         "User-Agent:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
     headlessoptions.add_argument("lang=ko_KR")
 
-    #driver = webdriver.Chrome(service=Service(DRIVER_PATH),options=headlessoptions)
-    driver = webdriver.Chrome(service=Service(DRIVER_PATH))
+    driver = webdriver.Chrome(service=Service(DRIVER_PATH),options=headlessoptions)
+    #driver = webdriver.Chrome(service=Service(DRIVER_PATH))
     driver.get(url)
 
 
